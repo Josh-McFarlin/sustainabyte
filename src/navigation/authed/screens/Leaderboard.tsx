6534b1ceb4768edc2e7607b05db9f36068d0a4f9
Josh McFarlin <josh@mcfarl.in>
2021-11-02 20:37:42 -0400

    Separate Leaderboard screen into tabs

--- src/navigation/authed/screens/Leaderboard.tsx
+++ src/navigation/authed/screens/Leaderboard.tsx
@@ -6,80 +6,40 @@ import {
   SafeAreaView,
   FlatList,
   Image,
-  Dimensions,
   TouchableOpacity,
 } from "react-native";
 import type { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { useQuery } from "react-query";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import type { TabNavParamList } from "../types";
-import { WaveLeft, WaveRight } from "../../../components/Waves";
 import { fetchUsers } from "../../../actions/user";
 import { User } from "../../../types/User";
 
 type PropTypes = BottomTabScreenProps<TabNavParamList, "Leaderboard">;
 
-enum TimeSpan {
-  TODAY,
-  WEEK,
+enum TabTypes {
+  LEADERBOARD,
+  CHALLENGES,
   MONTH,
 }
 
 const LeaderboardScreen: React.FC<PropTypes> = () => {
-  const [timeSpan, setTimeSpan] = React.useState<TimeSpan>(TimeSpan.WEEK);
+  const [curTab, setCurTab] = React.useState<TabTypes>(TabTypes.LEADERBOARD);
   const { data: users } = useQuery<User[], Error>(["users"], fetchUsers, {
     initialData: [],
   });
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.waveContainer}>
-          <WaveRight style={[styles.wave, styles.waveRight]} />
-          <WaveLeft style={[styles.wave, styles.waveLeft]} />
-          <View style={styles.rankContainer}>
-            <Text style={[styles.title, styles.marginBottom]}>Leaderboard</Text>
-            <View
-              style={[styles.hRow, styles.spaceAround, styles.marginBottom]}
-            >
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => setTimeSpan(TimeSpan.TODAY)}
-              >
-                <View
-                  style={
-                    timeSpan === TimeSpan.TODAY
-                      ? styles.selectedSpan
-                      : styles.spanContainer
-                  }
-                >
-                  <Text style={styles.spanText}>Today</Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setTimeSpan(TimeSpan.WEEK)}>
-                <View
-                  style={
-                    timeSpan === TimeSpan.WEEK
-                      ? styles.selectedSpan
-                      : styles.spanContainer
-                  }
-                >
-                  <Text style={styles.spanText}>Week</Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setTimeSpan(TimeSpan.MONTH)}>
-                <View
-                  style={
-                    timeSpan === TimeSpan.MONTH
-                      ? styles.selectedSpan
-                      : styles.spanContainer
-                  }
-                >
-                  <Text style={styles.spanText}>Month</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
+  const tabs = React.useMemo(
+    () => ({
+      [TabTypes.LEADERBOARD]: {
+        type: TabTypes.LEADERBOARD,
+        icon: ({ ...props }) => <Ionicons name="grid" {...props} />,
+        title: "Leaderboard",
+        data: users,
+        listProps: {},
+        PrimaryActions: () => (
+          <>
             <View style={[styles.hRow, styles.center, styles.marginBottom]}>
               <FontAwesome5 name="crown" size={32} color="#FFC601" />
             </View>
@@ -139,26 +99,179 @@ const LeaderboardScreen: React.FC<PropTypes> = () => {
                 </View>
               </View>
             </View>
+          </>
+        ),
+        Header: () => (
+          <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>This Week&apos;s Leaderboard</Text>
+          </View>
+        ),
+        Footer: () => null,
+        renderItem: ({ item, index }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.itemText}>{index + 1}</Text>
+            <Image style={styles.avatar} source={{ uri: item.avatarUrl }} />
+            <Text style={styles.itemText}>@{item.username}</Text>
+            <FontAwesome5
+              style={styles.crown}
+              name="crown"
+              size={18}
+              color="#FFC601"
+            />
+            <Text style={styles.itemText}>{item.score}</Text>
+          </View>
+        ),
+      },
+      [TabTypes.CHALLENGES]: {
+        type: TabTypes.CHALLENGES,
+        icon: ({ ...props }) => <FontAwesome5 name="map-pin" {...props} />,
+        title: "Challenges",
+        data: users,
+        listProps: {},
+        PrimaryActions: () => (
+          <>
+            <View style={[styles.hRow, styles.center, styles.marginBottom]}>
+              <FontAwesome5 name="crown" size={32} color="#FFC601" />
+            </View>
+            <View style={[styles.hRow, styles.center, styles.alignEnd]}>
+              <View style={[styles.vRow, styles.center]}>
+                <View style={styles.avatarShadow}>
+                  <Image
+                    style={[styles.topAvatar, styles.otherAvatars]}
+                    source={{
+                      uri: users?.[1]?.avatarUrl,
+                    }}
+                  />
+                </View>
+                <View style={styles.placeWrapper}>
+                  <LinearGradient
+                    colors={["#27F9FF", "#3C8D90"]}
+                    style={styles.placeCircle}
+                  >
+                    <Text style={styles.placeCircleText}>2</Text>
+                  </LinearGradient>
+                </View>
+              </View>
+              <View style={[styles.vRow, styles.center, styles.taZindex]}>
+                <View style={styles.avatarShadow}>
+                  <Image
+                    style={[styles.topAvatar, styles.firstAvatar]}
+                    source={{
+                      uri: users?.[0]?.avatarUrl,
+                    }}
+                  />
+                </View>
+                <View style={styles.placeWrapper}>
+                  <LinearGradient
+                    colors={["#27F9FF", "#3C8D90"]}
+                    style={styles.placeCircle}
+                  >
+                    <Text style={styles.placeCircleText}>1</Text>
+                  </LinearGradient>
+                </View>
+              </View>
+              <View style={[styles.vRow, styles.center]}>
+                <View style={styles.avatarShadow}>
+                  <Image
+                    style={[styles.topAvatar, styles.otherAvatars]}
+                    source={{
+                      uri: users?.[2]?.avatarUrl,
+                    }}
+                  />
+                </View>
+                <View style={styles.placeWrapper}>
+                  <LinearGradient
+                    colors={["#27F9FF", "#3C8D90"]}
+                    style={styles.placeCircle}
+                  >
+                    <Text style={styles.placeCircleText}>3</Text>
+                  </LinearGradient>
+                </View>
+              </View>
+            </View>
+          </>
+        ),
+        Header: () => (
+          <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>Ongoing Streaks</Text>
+          </View>
+        ),
+        Footer: () => (
+          <TouchableOpacity style={styles.center}>
+            <View style={styles.footerButtRad}>
+              <LinearGradient
+                style={styles.footerButton}
+                colors={["#00B7C3", "#255153"]}
+              >
+                <Text style={styles.footerText}>Extend a Challenge</Text>
+              </LinearGradient>
+            </View>
+          </TouchableOpacity>
+        ),
+        renderItem: ({ item, index }) => (
+          <View style={styles.listItem}>
+            <Text style={styles.itemText}>{index + 4}</Text>
+            <Image style={styles.avatar} source={{ uri: item.avatarUrl }} />
+            <Text style={styles.itemText}>@{item.username}</Text>
+            <FontAwesome5
+              style={styles.crown}
+              name="crown"
+              size={18}
+              color="#FFC601"
+            />
+            <Text style={styles.itemText}>{item.score}</Text>
+          </View>
+        ),
+      },
+    }),
+    [users]
+  );
+
+  const { data, PrimaryActions, Header, Footer, renderItem, listProps } =
+    tabs[curTab];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.gradContainer}>
+          <View style={styles.borderRad}>
+            <LinearGradient style={styles.grad} colors={["#3C8D90", "#08323F"]}>
+              <View>
+                <View
+                  style={[styles.hRow, styles.spaceAround, styles.marginBottom]}
+                >
+                  {Object.values(tabs).map((tab) => (
+                    <TouchableOpacity
+                      style={styles.typeButton}
+                      key={tab.type}
+                      onPress={() => setCurTab(tab.type)}
+                    >
+                      <View
+                        style={
+                          curTab === tab.type
+                            ? styles.selectedSpan
+                            : styles.spanContainer
+                        }
+                      >
+                        <Text style={styles.spanText}>{tab.title}</Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+                <PrimaryActions />
+              </View>
+            </LinearGradient>
           </View>
         </View>
         <FlatList
           style={styles.list}
-          data={users.slice(3)}
+          contentContainerStyle={styles.listContainer}
+          data={data}
           keyExtractor={(item) => item.id}
-          renderItem={({ item, index }) => (
-            <View style={styles.listItem}>
-              <Text style={styles.itemText}>{index + 4}</Text>
-              <Image style={styles.avatar} source={{ uri: item.avatarUrl }} />
-              <Text style={styles.itemText}>@{item.username}</Text>
-              <FontAwesome5
-                style={styles.crown}
-                name="crown"
-                size={18}
-                color="#FFC601"
-              />
-              <Text style={styles.itemText}>{item.score}</Text>
-            </View>
-          )}
+          renderItem={renderItem}
+          ListHeaderComponent={<Header />}
+          ListFooterComponent={<Footer />}
+          {...listProps}
         />
       </View>
     </SafeAreaView>
@@ -171,29 +284,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     position: "relative",
   },
-  waveContainer: {
-    width: Dimensions.get("screen").width,
-    height: 320,
+  gradContainer: {
+    backgroundColor: "#fff",
   },
-  rankContainer: {
-    zIndex: 12,
+  borderRad: {
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24,
+    overflow: "hidden",
   },
-  wave: {
-    position: "absolute",
-    top: 0,
-  },
-  waveLeft: {
-    left: -20,
-    zIndex: 2,
-  },
-  waveRight: {
-    right: 0,
-    zIndex: 1,
+  grad: {
+    paddingVertical: 32,
   },
   list: {
-    flex: 1,
+    // flex: 1,
+    // padding: 16,
+    // marginTop: 8,
+    // padding: 16,
+    // backgroundColor: "red",
+  },
+  listContainer: {
     padding: 16,
-    marginTop: 8,
   },
   listItem: {
     display: "flex",
@@ -296,15 +406,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#fff",
   },
+  typeButton: {
+    flex: 1,
+  },
   selectedSpan: {
-    backgroundColor: "#00726D",
-    paddingVertical: 4,
-    paddingHorizontal: 20,
-    borderRadius: 20,
+    borderBottomWidth: 3,
+    borderBottomColor: "#46FFD4",
+    alignItems: "center",
+    paddingBottom: 8,
   },
   spanContainer: {
-    paddingVertical: 4,
-    paddingHorizontal: 20,
+    borderBottomWidth: 3,
+    borderBottomColor: "#FDFFFF",
+    alignItems: "center",
+    paddingBottom: 8,
   },
   spanText: {
     fontSize: 16,
@@ -321,6 +436,27 @@ const styles = StyleSheet.create({
   },
   button: {
     zIndex: 30,
+  },
+  headerContainer: {
+    marginBottom: 16,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  footerButton: {
+    paddingVertical: 20,
+    paddingHorizontal: 48,
+  },
+  footerText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  footerButtRad: {
+    borderRadius: 64,
+    overflow: "hidden",
   },
 });
 
