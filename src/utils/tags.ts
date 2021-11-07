b6011cd9f24d90689e9a8e943d32894189d072f0
Josh McFarlin <josh@mcfarl.in>
2021-11-07 11:31:00 -0500

    Fix import to shared hashtags

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -1,15 +1,15 @@
 /* eslint-disable import/extensions */
 
 export const hashtagsToIcons = {
-  healthy: require("../../../assets/icons/healthy.png"),
-  vegan: require("../../../assets/icons/vegan.png"),
-  vegetarian: require("../../../assets/icons/healthy.png"),
-  yummy: require("../../../assets/icons/healthy.png"),
-  fancy: require("../../../assets/icons/healthy.png"),
-  delicious: require("../../../assets/icons/healthy.png"),
-  breakfast: require("../../../assets/icons/healthy.png"),
-  lunch: require("../../../assets/icons/healthy.png"),
-  dinner: require("../../../assets/icons/healthy.png"),
+  healthy: require("../../assets/icons/healthy.png"),
+  vegan: require("../../assets/icons/vegan.png"),
+  vegetarian: require("../../assets/icons/healthy.png"),
+  yummy: require("../../assets/icons/healthy.png"),
+  fancy: require("../../assets/icons/healthy.png"),
+  delicious: require("../../assets/icons/healthy.png"),
+  breakfast: require("../../assets/icons/healthy.png"),
+  lunch: require("../../assets/icons/healthy.png"),
+  dinner: require("../../assets/icons/healthy.png"),
 };
 
 export const hashtags = Object.keys(hashtagsToIcons);
